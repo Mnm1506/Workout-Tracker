a2061f93146a5f580381a2a979b75b52baa565bd
MNM <moussanm2014@gmail.com>
2021-03-02 10:31:09 -0600

    modified server.js file

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const mongojs = require("mongojs");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const db = "mongodb+srv://moussadb:Akbou2009@cluster0.vrfyl.mongodb.net/fitness_db?retryWrites=true&w=majority";
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness_db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+).then((result) => app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`)
+})),
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fitness_db',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+app.use(require("./routes/api-route"));
+app.use(require("./routes/html-route"));
 
-  app.use(require("./routes/api-route"));
-  app.use(require("./routes/html-route"));
 
-  app.listen(PORT, () => {
-     console.log(`App running on port ${PORT}!`)
-  });
-  
